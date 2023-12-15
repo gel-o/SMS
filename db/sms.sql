28417ee26dc7f1d017e467dfbcf193fb8577de66
Gelo <gelogelo6403@gmail.com>
2023-12-15 08:38:59 +0800

    updated sms

--- db/sms.sql
+++ db/sms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2023 at 02:53 PM
+-- Generation Time: Dec 15, 2023 at 01:38 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,43 +24,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `login` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_type` enum('admin','registrar','teacher','student') NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `middleInitial` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `birth_date` varchar(255) NOT NULL,
+  `sex` varchar(255) NOT NULL,
+  `Religion` varchar(255) NOT NULL,
+  `Civil Status` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `Guardian` varchar(255) NOT NULL,
+  `Contact Number` bigint(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `login`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `login` (`id`, `email`, `password`) VALUES
-(1, 'sample@gmail.com', 'sample');
+INSERT INTO `users` (`id`, `username`, `password`, `user_type`, `firstName`, `middleInitial`, `lastName`, `birth_date`, `sex`, `Religion`, `Civil Status`, `Address`, `Guardian`, `Contact Number`) VALUES
+(1, 'sample@gmail.com', 'sample123', 'student', 'Juan', 'D', 'La Cruz', 'January 1, 2003', 'Male', 'Catholic', 'Single', 'L3 B4 Diyan Lang Imus City Cavite', 'Jane La Cruz', 9323225424),
+(2, 'admin', 'admin123', 'admin', 'Coco', 'D', 'Martin', '', '', '', '', '', '', 0),
+(5, 'registrar', 'registrar123', 'registrar', 'Monkey', 'D', 'Lupin', '', '', '', '', '', '', 0),
+(6, 'teacher', 'teacher123', 'teacher', 'Jason', 'D', 'Rulo', '', '', '', '', '', '', 0),
+(9, 'james@gmail.com', 'james123', 'student', 'James', 'B', 'Bimbap', 'February 1, 2007', 'Female', 'Born Again', 'Single', 'L5 B2 Dun Lang Bacoor City Cavite', 'Kris Akiyes', 9154326789);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `login`
+-- Indexes for table `users`
 --
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `password` (`password`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `login`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
